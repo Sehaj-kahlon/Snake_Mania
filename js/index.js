cb2aca1ec2e02421e7f0a517bde2e53b2020e5d2
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-01-21 20:52:27 +0530

    speed change

--- js/index.js
+++ js/index.js
@@ -58,6 +58,7 @@ function gameEngine() {
     inputDir = { x: 0, y: 0 };
     alert("Game Over! Press any Key to play again!");
     score = 0;
+    speed = 4;
     scoreBox.innerHTML = "Score : " + score;
     snakeArr = [{ x: 13, y: 15 }];
   }
